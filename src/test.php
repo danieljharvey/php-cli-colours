d136db9e6bad5ad6e277275420703d0decb55462
Daniel Harvey <danieljamesharvey@gmail.com>
2017-04-10 14:37:40 +0100

    update test

--- src/test.php
+++ src/test.php
@@ -11,3 +11,7 @@ echo "What about some ".$bashColours->formatString("fucking blue shit",'blue'). 
 echo "I don't know.\n";
 
 echo "The shortcuts let us do ".$bashColours->blue("blue")." and ".$bashColours->red("red")." and ".$bashColours->lightMagenta("light magenta")." etc\n";
+
+$colour=new BashColours();
+
+echo $colour->red("red")." and ".$colour->yellow("yellow")." and ".$colour->lightMagenta("pink")." and ".$colour->green("green")."...\n";
